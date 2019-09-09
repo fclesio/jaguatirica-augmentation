d1a37fe9b9c96eef0db655e8d1728eee9cefb617
Flavio Clesio <flavioclesio@yahoo.com>
2019-09-09 12:55:22 +0200

    Logging and random noise
    - Random noise using `imgaug`
    - Logging for each file saved
    - Time tracking wrapper

--- jaguatirica.py
+++ jaguatirica.py
@@ -2,17 +2,17 @@
 # coding: utf-8
 
 # Load image processing Libraries
-import imageio
 import cv2 as cv
+import imageio
 import imgaug as ia
-import numpy as np
 import logging
+import multiprocessing as mp
+import numpy as np
 import os
+import shutil
 import time
 import uuid
-import shutil
 from imgaug import augmenters as iaa
-import multiprocessing as mp
 
 # Cleanup log file if exists
 try:
@@ -30,11 +30,15 @@ logger = logging.getLogger(__name__)
 # Definition of all constants and paths
 ia.seed(42)
 np.random.seed(seed=42)
-n_images = 15
+n_images = 30
+
+# Time tracker all script
+start_time_app = time.time()
+logger.info(f'WRAPPER - Start time: {time.strftime("%H:%M:%S" , time.gmtime(start_time_app))}')
 
 # Cleanup folders
 start_time = time.time()
-logger.info(f'DATA PREPARATION - Cleanup folders: {time.strftime("%H:%M:%S" , time.gmtime(start_time))}')
+logger.info(f'DATA PREPARATION - Start cleanup folders: {time.strftime("%H:%M:%S" , time.gmtime(start_time))}')
 
 # All image paths
 ROOT_DIR = os.getcwd()
@@ -50,7 +54,7 @@ for folder in working_folders:
 
 
 elapsed_time = time.time() - start_time
-logger.info(f'DATA PREPARATION - Cleanup folders - Elapsed time: {time.strftime("%H:%M:%S",time.gmtime(elapsed_time))}')
+logger.info(f'DATA PREPARATION - Finished cleanup - Elapsed time: {time.strftime("%H:%M:%S",time.gmtime(elapsed_time))}')
 
 logger.info(f'DATA PREPARATION - Source folder: {ROOT_DIR + SOURCE_DIR}')
 logger.info(f'DATA PREPARATION - Destination folder: {ROOT_DIR + DESTINATION_DIR}')
@@ -65,13 +69,15 @@ array_images = [s for s in array_files if "jpg" in s]
 if not isinstance(array_images, list):
     raise ValueError(f'DATA PREPARATION - Files not in a list. {time.strftime("%H:%M:%S",time.gmtime(time.time()))}')
 
+logger.info(f'DATA PREPARATION - Total Images: {len(array_images)}')
+
 
 def get_rotation(image):
     """
     Augmentation performs a initial rotation.
 
     Initial rotation where a human can read without effort.
-    As we're using 45 degrees left and right, I'll put 120
+    As we're using 60 degrees left and right, I'll put 120
     as the maximum combination of all rotations
 
     Parameters
@@ -80,9 +86,8 @@ def get_rotation(image):
         path for a .jpg file
 
     """
-
-    rotation_degrees = 45
-    rotation_combinations = 90
+    rotation_degrees = 60
+    rotation_combinations = 120
     img_source = ROOT_DIR + SOURCE_DIR + image
 
     logger.info(f'AUGMENTATION - Rotation - Starting {img_source} image')
@@ -97,7 +102,9 @@ def get_rotation(image):
         imageio.imwrite(ROOT_DIR + ROTATED_DIR + f'image_rotated_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_rotated_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_rotated_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Rotation - File: {save_path} saved')
 
     logger.info(f'AUGMENTATION - Rotation - {img_source} finished')
 
@@ -126,7 +133,9 @@ def get_gaussian_noise(image):
     images_aug = [seq.augment_image(image) for _ in range(n_images)]
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_gaussian_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_gaussian_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Gaussian - File: {save_path} saved')
 
 
 def get_crop_images(image):
@@ -151,7 +160,9 @@ def get_crop_images(image):
     images_aug = [seq.augment_image(image) for _ in range(n_images)]
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_crop_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_crop_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Crop - File: {save_path} saved')
 
 
 def get_flip_image(image):
@@ -164,7 +175,6 @@ def get_flip_image(image):
         path for a .jpg file
 
     """
-
     # Horizontally flip n% of the images
     horizontal_flip_degrees = 0.2
 
@@ -182,12 +192,14 @@ def get_flip_image(image):
     images_aug_verti = [seq_flip_verti.augment_image(image) for _ in range(n_images)]
 
     for i in range(0, len(images_aug_horiz) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_flip_horiz_{str(uuid.uuid4().hex)}.jpg',
-                        images_aug_horiz[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_flip_horiz_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug_horiz[i])
+        logger.info(f'AUGMENTATION - Flip Horizontal - File: {save_path} saved')
 
     for i in range(0, len(images_aug_verti) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_flip_verti_{str(uuid.uuid4().hex)}.jpg',
-                        images_aug_verti[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_flip_verti_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug_verti[i])
+        logger.info(f'AUGMENTATION - Flip Vertical - File: {save_path} saved')
 
 
 def get_blur_image(image):
@@ -200,7 +212,6 @@ def get_blur_image(image):
         path for a .jpg file
 
     """
-
     blur_sigma_lower = 0
     blur_sigma_upper = 2.5
 
@@ -211,7 +222,9 @@ def get_blur_image(image):
     images_aug = [seq.augment_image(image) for _ in range(n_images)]
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_blur_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_blur_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Blur - File: {save_path} saved')
 
 
 def get_normalization_image(image):
@@ -234,8 +247,9 @@ def get_normalization_image(image):
     images_aug = [seq.augment_image(image) for _ in range(n_images) ]
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_normalization_{str(uuid.uuid4().hex)}.jpg',
-                        images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_normalization_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Normalization - File: {save_path} saved')
 
 
 def get_sharpen_image(image):
@@ -262,7 +276,9 @@ def get_sharpen_image(image):
     images_aug = [seq.augment_image(image) for _ in range(n_images)]
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_sharpen_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_sharpen_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Sharpen - File: {save_path} saved')
 
 
 def get_channel_image(image):
@@ -285,7 +301,9 @@ def get_channel_image(image):
     images_aug = [seq.augment_image(image) for _ in range(n_images)]
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_channel_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_channel_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Disturb pixels - File: {save_path} saved')
 
 
 def get_gray_image(image):
@@ -308,7 +326,9 @@ def get_gray_image(image):
     images_aug = [seq.augment_image(image) for _ in range(n_images)]
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_grey_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_grey_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Gray Scale - File: {save_path} saved')
 
 
 def get_water_image(image):
@@ -332,7 +352,9 @@ def get_water_image(image):
     images_aug = [seq.augment_image(image) for _ in range(n_images)]
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_water_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_water_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Water effect - File: {save_path} saved')
 
 
 def get_negative_image(image):
@@ -357,7 +379,61 @@ def get_negative_image(image):
     images_aug = [seq.augment_image(image) for _ in range(n_images)]
 
     for i in range(0, len(images_aug) - 1):
-        imageio.imwrite(ROOT_DIR + DESTINATION_DIR + f'image_negative_{str(uuid.uuid4().hex)}.jpg', images_aug[i])
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_negative_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Negative Film effect - File: {save_path} saved')
+
+
+def get_random_noise(image):
+    """
+    1) horizontal flips
+    2) random crops
+    3) Small gaussian blur with random sigma
+    between 0 and 0.5.
+    But we only blur about 50% of all images.
+    4) Strengthen or weaken the contrast in each image.
+    6) Add gaussian noise. For 50% of all images,
+    we sample the noise once per pixel. For the other
+    50% of all images, we sample the noise per
+     pixel and channel. This can change the color
+     (not only brightness) of the pixels.
+    7) Make some images brighter and some darker.
+    8) In 20% of all cases, we sample the multiplier
+     once per channel, which can end up changing
+     the color of the images.
+    9) Apply affine transformations to each image.
+    Scale/zoom them, translate/move them,
+     rotate them and shear them.
+
+    Parameters
+    -----------
+    image: string
+        path for a .jpg file
+
+    """
+    image = cv.imread(ROOT_DIR + SOURCE_DIR + image, 1)
+
+    seq = iaa.Sequential([
+        iaa.Fliplr(0.5),
+        iaa.Crop(percent=(0, 0.1)),
+        iaa.Sometimes(0.5, iaa.GaussianBlur(sigma=(0, 0.5))),
+        iaa.ContrastNormalization((0.75, 1.5)),
+        iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5),
+        iaa.Multiply((0.8, 1.2), per_channel=0.1),
+        iaa.Affine(
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+            rotate=(-25, 25),
+            shear=(-8, 8)
+        )
+    ], random_order=True)
+
+    images_aug = [seq.augment_image(image) for _ in range(n_images)]
+
+    for i in range(0, len(images_aug) - 1):
+        save_path = ROOT_DIR + DESTINATION_DIR + f'image_random_noise_{str(uuid.uuid4().hex)}.jpg'
+        imageio.imwrite(save_path, images_aug[i])
+        logger.info(f'AUGMENTATION - Random Noise - File: {save_path} saved')
 
 
 def main_initial_rotation():
@@ -392,7 +468,6 @@ def main_apply_effects():
     pool = mp.Pool(mp.cpu_count())
     pool.map(get_gaussian_noise, array_images)
     pool.map(get_crop_images, array_images)
-    pool.map(get_flip_image, array_images)
     pool.map(get_blur_image, array_images)
     pool.map(get_normalization_image, array_images)
     pool.map(get_sharpen_image, array_images)
@@ -400,6 +475,7 @@ def main_apply_effects():
     pool.map(get_gray_image, array_images)
     pool.map(get_water_image, array_images)
     pool.map(get_negative_image, array_images)
+    pool.map(get_random_noise, array_images)
 
 
 # Time tracking
@@ -410,5 +486,9 @@ logger.info(f'AUGMENTATION - Start apply effects: {time.strftime("%H:%M:%S" , ti
 main_apply_effects()
 
 elapsed_time = time.time() - start_time
-logger.info(f'AUGMENTATION - Apply effects - Elapsed time: {time.strftime("%H:%M:%S" , time.gmtime(elapsed_time))}')
 logger.info(f'AUGMENTATION - End apply effects - Timestamp: {time.strftime("%H:%M:%S" , time.gmtime(time.time()))}')
+logger.info(f'AUGMENTATION - Apply effects - Elapsed time: {time.strftime("%H:%M:%S" , time.gmtime(elapsed_time))}')
+
+elapsed_time_app = time.time() - start_time_app
+logger.info(f'WRAPPER - Elapsed time: {time.strftime("%H:%M:%S" , time.gmtime(elapsed_time_app))}')
+logger.info(f'WRAPPER - End - Timestamp: {time.strftime("%H:%M:%S" , time.gmtime(time.time()))}')
